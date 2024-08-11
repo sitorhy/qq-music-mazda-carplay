6d60057b8c5253dc20ec7089c0a3898281f699ef
sitorhy <sitorhy@gmail.com>
2024-08-11 12:38:42 +0800

    feat: 增加请求响应体接受缓存设置

--- qq-music-server-lib/src/main/java/name/sitorhy/server/utils/WebClientUtils.java
+++ qq-music-server-lib/src/main/java/name/sitorhy/server/utils/WebClientUtils.java
@@ -4,13 +4,14 @@ import name.sitorhy.server.session.RequestHeadersSession;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilder;
 
 import java.net.URI;
 import java.util.Map;
-import java.util.function.Function;
 
 public class WebClientUtils {
+    // 扩大接收缓存 4MB； 默认 256 KB 部分请求会溢出
+    private final static int RESPONSE_BUFFER_SIZE = 1024 * 1024 * 4;
+
     static private void setDefaultHeaders(HttpHeaders httpHeaders, RequestHeadersSession requestHeadersSession) {
         httpHeaders.set("Accept-Language", requestHeadersSession.getAcceptLanguage());
         httpHeaders.set("Origin", requestHeadersSession.getOrigin());
@@ -27,14 +28,30 @@ public class WebClientUtils {
 
     public static WebClient.RequestHeadersSpec<? extends WebClient.RequestHeadersSpec<?>> create(RequestHeadersSession requestHeadersSession, HttpMethod httpMethod, URI uri) {
         WebClient webClient = WebClient.create();
-        return webClient.method(httpMethod)
+        return webClient
+                .mutate()
+                .codecs(
+                        configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(RESPONSE_BUFFER_SIZE)
+                )
+                .build()
+                .method(httpMethod)
                 .uri(uri)
                 .headers(httpHeaders -> setDefaultHeaders(httpHeaders, requestHeadersSession));
     }
 
     public static WebClient.RequestHeadersSpec<? extends WebClient.RequestHeadersSpec<?>> create(RequestHeadersSession requestHeadersSession, HttpMethod httpMethod, URI uri, Object bodyValue) {
         WebClient webClient = WebClient.create();
-        return webClient.method(httpMethod)
+        return webClient
+                .mutate()
+                .codecs(
+                        configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(RESPONSE_BUFFER_SIZE)
+                )
+                .build()
+                .method(httpMethod)
                 .uri(uri)
                 .bodyValue(bodyValue)
                 .headers(httpHeaders -> setDefaultHeaders(httpHeaders, requestHeadersSession));
@@ -42,14 +59,30 @@ public class WebClientUtils {
 
     public static WebClient.RequestHeadersSpec<? extends WebClient.RequestHeadersSpec<?>> create(RequestHeadersSession requestHeadersSession, HttpMethod httpMethod, String uri, Map<String, Object> uriVariables) {
         WebClient webClient = WebClient.create();
-        return webClient.method(httpMethod)
+        return webClient
+                .mutate()
+                .codecs(
+                        configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(RESPONSE_BUFFER_SIZE)
+                )
+                .build()
+                .method(httpMethod)
                 .uri(uri, uriVariables)
                 .headers(httpHeaders -> setDefaultHeaders(httpHeaders, requestHeadersSession));
     }
 
     public static WebClient.RequestHeadersSpec<? extends WebClient.RequestHeadersSpec<?>> create(RequestHeadersSession requestHeadersSession, HttpMethod httpMethod, String uri, Object bodyValue) {
         WebClient webClient = WebClient.create();
-        return webClient.method(httpMethod)
+        return webClient
+                .mutate()
+                .codecs(
+                        configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(RESPONSE_BUFFER_SIZE)
+                )
+                .build()
+                .method(httpMethod)
                 .uri(uri)
                 .bodyValue(bodyValue)
                 .headers(httpHeaders -> setDefaultHeaders(httpHeaders, requestHeadersSession));
