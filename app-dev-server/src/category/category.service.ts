4b921e7b5dc9c29ef4654da599a742fffed478ce
sitorhy <sitorhy@gmail.com>
2025-04-08 09:11:08 +0800

    返回格式

--- app-dev-server/src/category/category.service.ts
+++ app-dev-server/src/category/category.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { getAllPlaylists } from '../data/playlists';
 import { getAllTagGroups, getTagsByGroupId } from '../data/tags';
+import { PaginationResponse, Response } from '../model/response';
+import { Tag, TagGroup } from '../model/tag';
+import { Playlist } from '../model/playlist';
 
 @Injectable()
 export class CategoryService {
-  async getRecommendFeed(pageNo: number, pageSize: number) {
-    return getTagsByGroupId(0x67);
+  async getRecommendFeed(pageNo: number, pageSize: number): Promise<Response<Tag[]>> {
+    const tags = await getTagsByGroupId(0x67);
+    return new Response<Tag[]>({
+      data: tags.slice(pageNo * (pageNo - 1), pageNo * pageSize),
+    });
   }
 
-  async getHotCategory() {
-    return getTagsByGroupId(0x68);
+  async getHotCategory(): Promise<Response<Tag[]>> {
+    const tags = await getTagsByGroupId(0x68);
+    return new Response<Tag[]>({
+      data: tags,
+    });
   }
 
-  async getAllTag() {
-    return getAllTagGroups();
+  async getAllTag(): Promise<Response<TagGroup[]>> {
+    const groups= await getAllTagGroups();
+    return new Response<TagGroup[]>({
+        data: groups,
+    });
   }
 
-  async getPlayListCategory(categoryId: number, pageNo: number, pageSize: number) {
-    return getAllPlaylists();
+  async getPlayListCategory(categoryId: number, pageNo: number, pageSize: number): Promise<PaginationResponse<Playlist[]>> {
+    const testPlaylists = await getAllPlaylists();
+    return new PaginationResponse<Playlist[]>({
+      data: testPlaylists.slice(pageSize * (pageNo - 1), pageSize * pageNo),
+      pageNo: pageNo,
+      pageSize: pageSize,
+      total: testPlaylists.length,
+    });
   }
 }
